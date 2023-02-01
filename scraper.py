e222e23ee3d5887211656e3cc21cac6b0a378429
Abhinandan Shrestha <shtabhi@gmail.com>
2023-02-02 00:45:26 +0545

    Basic Landing for thehimalayantimes

--- scraper.py
+++ scraper.py
@@ -4,26 +4,25 @@ import re
 import csv
 
 #Configure beautiful soup
-url="https://news.sky.com/"
+url="https://thehimalayantimes.com/morearticles/Kathmandu?pgno=1"
 htmldocs=requests.get(url).content
 soup = BeautifulSoup(htmldocs,'html.parser')
 
 #Create csv ready data
 data=[]
-container = soup.find_all('div',class_='sdc-site-tile__body-main')
+container = soup.find_all('article',class_='row animate-box fadeInUp animated-fast')
 for item in container:
-    title = item.find('a',class_='sdc-site-tile__tag-link')
-    if title!=None:
-        headline = item.find('span',class_='sdc-site-tile__headline-text')
-        category=re.sub(r'\s+', '',title.text)
-        headline = headline.text
-        # print(category,headline)
-        a={'category':category,'headline':headline}
-        data.append(a)
+    headline = item.find('h3',class_='alith_post_title HideMobile').text
+    body =  item.find('div',class_='alith_post_except HideMobile fontsize14height40').text
+    body=re.sub(r"^\s+|\s+$", "", body)
+    location = item.find('span',class_="section").text
+    time_published =  item.find('span', class_="meta_date").text
+    a={'headline':headline,'body':body,'location':location,'time_published':time_published}
+    data.append(a)  
 # print(data)
 
 # Save file to csv for further analysis
 with open('scraptedData.csv', 'w') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames = ['category','headline'])
+    writer = csv.DictWriter(csvfile, fieldnames = ['headline','body','location','time_published'])
     writer.writeheader()
     writer.writerows(data)
